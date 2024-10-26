b7006c872cd07bc42a961033bd82ccebfeb6a4d3
Kirill <kirill0392@gmail.com>
2024-10-26 14:58:09 +0300

    add 3rd HW

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,16 +1,17 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+	callBack: () => void; // НУЖНО ПРОТИПИЗИРОВАТЬ any
+	name: string; // НУЖНО ПРОТИПИЗИРОВАТЬ any
 };
 
 export const Button = (props: ButtonPropsType) => {
-  const callBackHandler = () => {
-    // НУЖНО ДОПИСАТЬ
-  };
+	const callBackHandler = () => {
+		// НУЖНО ДОПИСАТЬ
+    props.callBack()
+	};
 
-  return (
-    <button id={'hw04-button'} onClick={callBackHandler}>
-      {props.name}
-    </button>
-  );
+	return (
+		<button id={"hw04-button"} onClick={callBackHandler}>
+			{props.name}
+		</button>
+	);
 };
